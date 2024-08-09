ef41e291a788edd7989a77cca99d4b0155954765
mmilten <71413695+mmilten@users.noreply.github.com>
2024-08-09 02:02:27 -0400

    Add all files for webpage

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 const forms = document.querySelector(".forms"),
-      pwShowHide = document.querySelectorAll(".eye-icon"),
-      links = document.querySelectorAll(".link");
+    pwShowHide = document.querySelectorAll(".eye-icon"),
+    links = document.querySelectorAll(".link");
 
 pwShowHide.forEach(eyeIcon => {
     eyeIcon.addEventListener("click", () => {
         let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-        
+
         pwFields.forEach(password => {
-            if(password.type === "password"){
+            if (password.type === "password") {
                 password.type = "text";
                 eyeIcon.classList.replace("bx-hide", "bx-show");
                 return;
@@ -15,13 +15,50 @@ pwShowHide.forEach(eyeIcon => {
             password.type = "password";
             eyeIcon.classList.replace("bx-show", "bx-hide");
         })
-        
+
     })
-})      
+})
 
 links.forEach(link => {
     link.addEventListener("click", e => {
-       e.preventDefault(); //preventing form submit
-       forms.classList.toggle("show-signup");
+        e.preventDefault(); //preventing form submit
+        forms.classList.toggle("show-signup");
     })
 })
+
+/*
+ * Create form to request access token from Google's OAuth 2.0 server.
+ */
+
+function signIn() {
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+    var form = document.createElement('form');
+    form.setAttribute('method', 'GET');
+    form.setAttribute('action', oauth2Endpoint);
+
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {
+        'client_id': '489676956233-aadsc1iqb6v7p4jdrl9ldgjrrsiccb8c.apps.googleusercontent.com',
+        'redirect_uri': 'https://688d-71-244-159-124.ngrok-free.app/webpage/index.php',
+        'response_type': 'token',
+        'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+        'include_granted_scopes': 'true',
+        'state': 'pass-through value'
+    };
+
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', p);
+        input.setAttribute('value', params[p]);
+        form.appendChild(input);
+    }
+
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
+}
