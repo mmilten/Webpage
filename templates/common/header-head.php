0588b5e4a8818f282883f2cec22c4963e55bee05
mmilten <71413695+mmilten@users.noreply.github.com>
2024-09-02 00:59:09 -0400

    Add new folder

---
+++ templates/common/header-head.php
@@ -0,0 +1,12 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- CSS -->
+<link href="https://fonts.googleapis.com/css2?family=Reem+Kufi+Ink&display=swap" rel="stylesheet">
+<!-- Boxicons CSS -->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<!-- JustValidate -->
+<script src=" https://unpkg.com/just-validate@latest/dist/just-validate.production.min.js" defer>
+</script>
+<style>
+    <?php include('../css/header.css'); ?>
+</style>
