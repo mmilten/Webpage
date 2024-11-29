dd02010da1808237628d938f6dec2ec25d97985c
mmilten <71413695+mmilten@users.noreply.github.com>
2024-11-29 05:21:47 -0500

    Add Font Awesome CSS

--- templates/common/header-head.php
+++ templates/common/header-head.php
@@ -1,6 +1,9 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- CSS -->
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<!-- Google -->
 <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi+Ink&display=swap" rel="stylesheet">
 <!-- Boxicons CSS -->
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
